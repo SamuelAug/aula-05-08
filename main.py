55b70c501119a64ea209ae7c355b845d472d175f
SamuelAug1 <mlksaam@gmail.com>
2020-08-12 23:46:17 +0000

    arvore de decisoes

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from sklearn import tree
+import matplotlib .pyplot as plt
 
+X_names = ['peso', 'textura']
 X = [
-  [150,0],
-  [170,0],
-  [140,1],
-  [130,1]
+  [150, 0],
+  [170, 0],
+  [140, 1],
+  [130, 1],
+  [90, 0],
+  [80, 0],
+  [100, 1],
+  [105, 1]
 ]
 
-y = [0, 0, 1, 1]
+y_names = ['laranja', 'maça', 'limão', 'banana']
+y = [0, 0, 1, 1, 2, 2, 3, 3]
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, y)
 
-predict = clf.predict([
-  [135, 1],
-  [160, 0]
-])
+fig = plt.figure()
 
-print(predict)
+tree.plot_tree(
+  clf,
+  feature_names=X_names,
+  class_names = y_names,
+  filled=True,
+  impurity=False,
+  rounded=True
+  )
+
+plt.savefig('frutas.png')
